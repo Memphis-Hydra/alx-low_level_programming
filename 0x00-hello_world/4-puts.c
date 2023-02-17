5caa6be083be44c1ab706b92f2bbb3b9c855fd60
Memphis-Hydra <Iliass.khalki@gmail.com>
2023-02-16 18:26:20 -0800

    Task4: Write a C program that prints exactly a string , Followed by new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - C program that prints an Exact String "Programming is like building a multilingual puzzle" usings puts
- *
- * no Use for Printf
+ * main - C program that prints ""Programming is like building a multilingual puzzle " with puts
  *
  * Return: Always 0 (Success)
  */
-
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
