a4723b26b023ed2b6b61ed3d7f64835090d7456e
Memphis-Hydra <Iliass.khalki@gmail.com>
2023-02-16 18:42:12 -0800

    Task 5 :

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - C program that prints exactly a String, followed by a new line, using printf
- *
- * Return: Always 0 (Success)
- */
+*main - C program to print a string
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
