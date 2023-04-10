164545f0b22afe622a291d8f6bb1e4c5c4ccc169
Memphis-Hydra <Iliass.khalki@gmail.com>
2023-04-10 13:44:43 -0700

    Initial commit

--- 0x15-file_io/1-create_textfile.c
+++ 0x15-file_io/1-create_textfile.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int f, l, write_bytes;
+
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f == -1)
 		return (-1);
-
-	close(o);
-
+	if (text_content == NULL)
+	{
+		close(f);
+		return (1);
+	}
+	for (l = 0; text_content[l]; l++)
+	write_bytes = write(f, text_content, l);
+	if (write_bytes == -1)
+	{
+		close(f);
+		return (-1);
+	}
+	close(f);
 	return (1);
 }
-
